1c2442078853c3ecab8741a714bf246345c10869
dchavezf <dchavezf@yahoo.com>
2022-07-19 11:32:29 -0500

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry meal')
+streamlit.text('Kale, Spinache & Roclie Smotthie')
+streamlit.text('Hard-Boiled Free-Ranged Egg')
 
