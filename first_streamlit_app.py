e8d14019f62fa51c982f9a2077f2b4b2da2d8114
dchavezf <dchavezf@yahoo.com>
2022-07-19 11:38:16 -0500

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry meal')
-streamlit.text('🥗Kale, Spinache & Roclie Smotthie')
+streamlit.text('🥗Kale, Spinache & Rocky Smotthie')
 streamlit.text('🥑🍞Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
