dbbd620a2397257d6bdbb60b667ef523ab2acb75
dchavezf <dchavezf@yahoo.com>
2022-07-25 11:29:34 -0500

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-#import snowflake.connector
+import snowflake.connector
 
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
